f0e650a306422f1452580a83ad5a9ba06934a3b5
maikelsi <83016522+maikelsi@users.noreply.github.com>
2024-08-01 20:17:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("SEU_HTML.html", titulo=TITULO)
+    return render_template("site.html", titulo=TITULO)
 
 
 asgi_app = WsgiToAsgi(app)
